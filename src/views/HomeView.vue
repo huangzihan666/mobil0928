6e5c9a8db7c405d2e817ae2058ae2561d3541926
huangzihan666 <1339745306@qq.com>
2022-09-28 15:36:12 +0800

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home">
+    <div class="nav">
+      <div class="pic">
+        <img src="../../public/img/img_02.png" alt="">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import {home_http} from "@/api/httpHome.js"
+  // console.log();
+  async function gethome(){
+    let {data} = await home_http()
+    console.log(data);
+  }
+  gethome()
+  
+</script>
+<style lang="scss" scoped>
+  .home{
+    .nav{
+      .dic{
+        width: 750px;
+        height: 296px;
+        background: red;
+        img{
+          width: 100%;
+          height: 100%;
+        }
+      }
+    }
+  }
+</style>
---
+++ src/views/MyCar.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        购物车
+    </div>
+</template>
+<script lang="ts" setup>
+
+</script>
+<style lang="scss" scoped>
+
+    
+</style>
